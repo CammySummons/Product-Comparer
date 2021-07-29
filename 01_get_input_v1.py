eab868fad0f61e48db07e75d3f3cf98e99af11e6
CammySummons <64822645+CammySummons@users.noreply.github.com>
2021-07-30 09:35:17 +1200

    Update 01_get_input_v1.py

--- 01_get_input_v1.py
+++ 01_get_input_v1.py
@@ -5,6 +5,7 @@ Version 1
 """
 
 another_product = ""
+convert_to_grams = False
 
 
 # Number checking function
@@ -22,25 +23,26 @@ def num_check(question, error):
 
 
 # Abbreviation lists
-teaspoon = ["tsp", "teaspoon", "t", "teaspoons"]
-tablespoon = ["tbs", "tbsp", "tablespoon", "T", "Tbsp", "tablespoons"]
-ounce = ["oz", "fluid ounce", "fl oz", "ounce", "ounces", "oz."]
-cup = ["c", "cup", "cups"]
-pint = ["p", "pt", "fl pt", "pint", "pints", "pt."]
-quart = ["q", "qt", "fl", "qt.", "quart", "quarts"]
-ml = ["milliliter", "millilitre", "cc", "mL",
-      "milliliters", "millilitres", "mls"]
-litre = ["liter", "litre", "L", "liters", "litres"]
-decilitre = ["deciliter", "decilitre", "dL", "deciliters", "decilitres"]
-pound = ["lb", "lbs", "#", "pound", "pounds", "lb.", "lbs."]
+# teaspoon = ["tsp", "teaspoon", "t", "teaspoons"]
+# tablespoon = ["tbs", "tbsp", "tablespoon", "T", "Tbsp", "tablespoons"]
+# ounce = ["oz", "fluid ounce", "fl oz", "ounce", "ounces", "oz."]
+# # cup = ["c", "cup", "cups"]
+# # pint = ["p", "pt", "fl pt", "pint", "pints", "pt."]
+# # quart = ["q", "qt", "fl", "qt.", "quart", "quarts"]
+# ml = ["milliliter", "millilitre", "cc", "mL",
+#       "milliliters", "millilitres", "mls"]
+# litre = ["liter", "litre", "L", "liters", "litres"]
+# decilitre = ["deciliter", "decilitre", "dL", "deciliters", "decilitres"]
+# pound = ["lb", "lbs", "#", "pound", "pounds", "lb.", "lbs."]
 grams = ["g", "gram", "gms", "grams", "gm"]
 kilograms = ["kg", "kilogram", "kilograms"]
-all_units = [teaspoon, tablespoon, ounce, cup, pint, quart, ml,
-             litre, decilitre, pound, grams, kilograms]
+all_units = [grams, kilograms]
 
 
 # String checking function
 def str_check(question, error):
+    global convert_to_grams
+    convert_to_grams = False
     in_list = False
     valid = False
     while not valid:
@@ -50,6 +52,8 @@ def str_check(question, error):
                 if response in unit_list:
                     in_list = True
             if in_list:
+                if response in kilograms:
+                    convert_to_grams = True
                 return response
             else:
                 print(error)
@@ -76,8 +80,16 @@ while another_product.upper() != "YES":
     another_product = ""
     product_name.append(input("Enter the product name: "))
     product_unit.append(str_check("Enter the unit of measurement: ", "Error"))
-    product_mass_volume.append(num_check("Enter the product mass/volume: ",
-                                         "!!Please enter a number above 0!!"))
+
+    if convert_to_grams:
+        product_mass_volume.append(num_check("Enter the product mass/volume: ",
+                                             "!!Please enter a number "
+                                             "above 0!!")*1000)
+    else:
+        product_mass_volume.append(num_check("Enter the product mass/volume: ",
+                                             "!!Please enter a number "
+                                             "above 0!!"))
+
     product_price.append(num_check("Enter the product price: $",
                                    "!!Please enter a price above $0!!"))
 
