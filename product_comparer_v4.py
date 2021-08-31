41f006a66647bc8d6d1a537ad063617648722150
CammySummons <64822645+CammySummons@users.noreply.github.com>
2021-08-31 15:37:02 +1200

    Updates

--- product_comparer_v4.py
+++ product_comparer_v4.py
@@ -5,32 +5,7 @@ Version 4
 30/08/2021
 """
 
-# List Declaration
-price_per_unit_list = []  # Contains the price/mass for each product
-price_per_unit = []
-prices = []
-cheapest = []  # Contains the least expensive item
-expensive = []  # Contains the most expensive item
-product_details = []
-all_product_details = []  # Contains all products and their details
-too_expensive_id_list = [""]  # If an item's cost > the users money on hand,
-# it will be added to this list
-
-# Setting variables
-global price
-another_product = ""
-recommended_purchase = ""
-convert_to_grams = False
-convert_to_litres = False
-convert_to_kg = False
-convert_to_ml = False
-
-# Zeroing variables
-mass = 0
-count = 0
-money = 0
-product_entries = 0
-
+loop = "Yes"  # Allows program to run
 
 # Abbreviation lists
 ml = ["milliliter", "millilitre", "cc", "mL",
@@ -107,7 +82,6 @@ def blank_checker(question, error):
             return response
 
 
-# Main Routine
 # Welcome + Information
 print("                ******Welcome to the Product Comparer!******\n")
 print("                ----------------Information----------------"
@@ -122,164 +96,197 @@ print("                ----------------Information----------------"
 print("//Please enter the following details to proceed "
       "with the product comparisons\\\\ \n")
 
-# Asking for money on hand
-while money < 10:
-    money = num_check("Please enter how much money you would like to spend: $",
-                      "!!You must enter a number!!\n")
-    if money < 10:
-        print("!!You must have at least $10!!\n")
 
-# Component 1
-# Asking for details and doing error checking
-while another_product.upper() != "YES":
+# Main Routine
+while loop != "NO":
+    # List Declaration
+    price_per_unit_list = []  # Contains the price/mass for each product
+    price_per_unit = []
+    prices = []
+    cheapest = []  # Contains the least expensive item
+    expensive = []  # Contains the most expensive item
     product_details = []
+    all_product_details = []  # Contains all products and their details
+    too_expensive_id_list = [""]  # If an item's cost > the users money on hand,
+    # it will be added to this list
+
+    # Setting variables
+    global price
     another_product = ""
+    recommended_purchase = ""
+    convert_to_grams = False
+    convert_to_litres = False
+    convert_to_kg = False
+    convert_to_ml = False
 
-    product_details.append(blank_checker("\nEnter the product name: ",
-                                         "!!You cannot leave this blank!!\n"))
-
-    product_details.append(str_check("Enter the unit of measurement: ",
-                                     "!!Enter a valid measurement (you cannot "
-                                     "compare mass to volume)!!\n"))
-
-    if convert_to_grams:
-        product_details.append(num_check("Enter the product mass/volume: ",
-                                         "!!Please enter a number "
-                                         "above 0!!\n"))
-        if litre in all_units:
-            all_units.remove(litre)
-            all_units.remove(ml)
-
-    elif convert_to_kg:
-        product_details.append(num_check("Enter the product mass/volume: ",
-                                         "!!Please enter a number "
-                                         "above 0!!\n")*1000)
-        if litre in all_units:
-            all_units.remove(litre)
-            all_units.remove(ml)
-
-    elif convert_to_litres:
-        product_details.append(num_check("Enter the product mass/volume: ",
-                                         "!!Please enter a number "
-                                         "above 0!!\n")*1000)
-        if grams in all_units:
-            all_units.remove(grams)
-            all_units.remove(kilograms)
-
-    elif convert_to_ml:
-        product_details.append(num_check("Enter the product mass/volume: ",
-                                         "!!Please enter a number "
-                                         "above 0!!\n"))
-        if grams in all_units:
-            all_units.remove(grams)
-            all_units.remove(kilograms)
-
-    product_details.append(num_check("Enter the product price: $",
-                                     "!!Please enter a price above $0!!\n"))
-
-    all_product_details.append(product_details)
-    product_entries += 1
-
-    if product_entries > 1:
-        another_product = input("Would you like to compare the products yet? ")
-        while another_product.upper().strip() != "NO" and \
-                another_product.upper().strip() != "YES":
-            another_product = input("!!Enter either 'Yes' or 'No'!!\nWould you"
-                                    " like to compare the products yet? ")
-    else:
-        another_product = "NO"
-
-
-# Component 2
-# Extracting mass and price from all_product_details to get price_per_unit
-for product in all_product_details:
+    # Zeroing variables
     mass = 0
-    price = 0
     count = 0
-    for detail in product:
-        if count == 2:
-            mass = detail
-        elif count == 3:
-            price = detail
-        count += 1
+    money = 0
+    product_entries = 0
 
-    if price > money:
-        too_expensive_id_list.append(price)
-        price = 0
+    # Asking for money on hand
+    while money < 10:
+        money = num_check("Please enter how much money you would like to spend: $",
+                          "!!You must enter a number!!\n")
+        if money < 10:
+            print("!!You must have at least $10!!\n")
+
+    # Component 1
+    # Asking for details and doing error checking
+    while another_product.upper() != "YES":
+        product_details = []
+        another_product = ""
+
+        product_details.append(blank_checker("\nEnter the product name: ",
+                                             "!!You cannot leave this blank!!\n"))
+
+        product_details.append(str_check("Enter the unit of measurement: ",
+                                         "!!Enter a valid measurement (you cannot "
+                                         "compare mass to volume)!!\n"))
+
+        if convert_to_grams:
+            product_details.append(num_check("Enter the product mass/volume: ",
+                                             "!!Please enter a number "
+                                             "above 0!!\n"))
+            if litre in all_units:
+                all_units.remove(litre)
+                all_units.remove(ml)
+
+        elif convert_to_kg:
+            product_details.append(num_check("Enter the product mass/volume: ",
+                                             "!!Please enter a number "
+                                             "above 0!!\n")*1000)
+            if litre in all_units:
+                all_units.remove(litre)
+                all_units.remove(ml)
+
+        elif convert_to_litres:
+            product_details.append(num_check("Enter the product mass/volume: ",
+                                             "!!Please enter a number "
+                                             "above 0!!\n")*1000)
+            if grams in all_units:
+                all_units.remove(grams)
+                all_units.remove(kilograms)
+
+        elif convert_to_ml:
+            product_details.append(num_check("Enter the product mass/volume: ",
+                                             "!!Please enter a number "
+                                             "above 0!!\n"))
+            if grams in all_units:
+                all_units.remove(grams)
+                all_units.remove(kilograms)
+
+        product_details.append(num_check("Enter the product price: $",
+                                         "!!Please enter a price above $0!!\n"))
+
+        all_product_details.append(product_details)
+        product_entries += 1
+
+        if product_entries > 1:
+            another_product = input("Would you like to compare the products yet? ")
+            while another_product.upper().strip() != "NO" and \
+                    another_product.upper().strip() != "YES":
+                another_product = input("!!Enter either 'Yes' or 'No'!!\nWould you"
+                                        " like to compare the products yet? ")
+        else:
+            another_product = "NO"
+
+    # Component 2
+    # Extracting mass and price from all_product_details to get price_per_unit
+    for product in all_product_details:
         mass = 0
+        price = 0
+        count = 0
+        for detail in product:
+            if count == 2:
+                mass = detail
+            elif count == 3:
+                price = detail
+            count += 1
+
+        if price > money:
+            too_expensive_id_list.append(price)
+            price = 0
+            mass = 0
+        else:
+            price_per_unit = price / mass
+            product.append(price_per_unit)
+            price_per_unit_list.append(price_per_unit)
+
+    if len(price_per_unit_list) > 0:
+        price_per_unit_list = sorted(price_per_unit_list)
+        best_buy_price = price_per_unit_list[0]
+        for product in all_product_details:
+            if best_buy_price in product:
+                recommended_purchase = product[0]
     else:
+        i = 0
+        low_i = 0
+        for product in all_product_details:
+            if i == 0:
+                lowest = product[3]/product[2]
+            if product[3]/product[2] < lowest:
+                lowest = product[3]/product[2]
+                low_i = i
+            i += 1
+        recommended_purchase = "You can't afford any of these items but the " \
+                               "best value item is {}"\
+            .format(all_product_details[low_i][0])
+
+    # Component 3
+    # Ranking from most expensive to least expensive
+    for product in all_product_details:
+        prices.append(product[3])
+
+    prices = sorted(prices)
+    prices = [prices[0], prices[-1]]
+    for product in all_product_details:
+        for detail in product:
+            for price in prices:
+                if prices[0] in product:
+                    cheapest = product[0]
+                if prices[-1] in product:
+                    expensive = product[0]
+
+    # Component 4 - summary
+    print("\n\n------Products Entered------\n")
+
+    # Extracting details from all_product_details
+    count = 0
+    for product in all_product_details:
+        for detail in product:
+            if count == 0:
+                print("Item name: " + str(detail))
+            elif count == 2:
+                mass = detail
+            elif count == 3:
+                print("Cost: $" + str("{:.2f}".format(detail)))
+                price = detail
+
+            count += 1
+            for ID in too_expensive_id_list:
+                if str(ID) == str(price):
+                    if count == 4:
+                        count = 0
+                else:
+                    if count == 5:
+                        count = 0
+
         price_per_unit = price / mass
-        product.append(price_per_unit)
-        price_per_unit_list.append(price_per_unit)
+        print("Average unit price: ${:.4f}\n".format(price_per_unit))
 
-if len(price_per_unit_list) > 0:
-    price_per_unit_list = sorted(price_per_unit_list)
-    best_buy_price = price_per_unit_list[0]
-    for product in all_product_details:
-        if best_buy_price in product:
-            recommended_purchase = product[0]
-else:
-    i = 0
-    low_i = 0
-    for product in all_product_details:
-        if i == 0:
-            lowest = product[3]/product[2]
-        if product[3]/product[2] < lowest:
-            lowest = product[3]/product[2]
-            low_i = i
-        i += 1
-    recommended_purchase = "You can't afford any of these items but the " \
-                           "best value item is {}"\
-        .format(all_product_details[low_i][0])
+    print("\n------Cheapest and Most Expensive Products------\n")
+    print("Cheapest Product: {}\nMost Expensive Product: {}\n".format
+          (cheapest, expensive))
 
+    print("\n------Recommended Product Purchase in Terms of Value------\n")
+    print("Recommended Product Purchase (Best value affordable item): {}"
+          .format(recommended_purchase))
 
-# Component 3
-# Ranking from most expensive to least expensive
-for product in all_product_details:
-    prices.append(product[3])
-
-prices = sorted(prices)
-prices = [prices[0], prices[-1]]
-for product in all_product_details:
-    for detail in product:
-        for price in prices:
-            if prices[0] in product:
-                cheapest = product[0]
-            if prices[-1] in product:
-                expensive = product[0]
-
-
-# Component 4 - summary
-print("\n\n------Products Entered------\n")
-
-# Extracting details from all_product_details
-count = 0
-for product in all_product_details:
-    for detail in product:
-        if count == 0:
-            print("Item name: " + str(detail))
-        elif count == 2:
-            mass = detail
-        elif count == 3:
-            print("Cost: $" + str("{:.2f}".format(detail)))
-            price = detail
-
-        count += 1
-        for ID in too_expensive_id_list:
-            if str(ID) == str(price):
-                if count == 4:
-                    count = 0
-            else:
-                if count == 5:
-                    count = 0
-
-    price_per_unit = price / mass
-    print("Average unit price: ${:.4f}\n".format(price_per_unit))
-
-print("\n------Cheapest and Most Expensive Products------\n")
-print("Cheapest Product: {}\nMost Expensive Product: {}\n".format
-      (cheapest, expensive))
-
-print("\n------Recommended Product Purchase in Terms of Value------\n")
-print("Recommended Product Purchase (Best value affordable item): {}"
-      .format(recommended_purchase))
+    loop = input("\n\nWould you like to use the program again? (Type 'Yes' to"
+                 " use again or 'No' to exit): ").upper().strip()
+    while loop.upper().strip() != "NO" and \
+            loop.upper().strip() != "YES":
+        loop = input("!!Enter either 'Yes' or 'No'!!\nWould you like to use "
+                     "the program again? (Type 'Yes' to use again or 'No' to exit): ")
